49e223f8b256b47746413ab06d003ed16edc8f2a
himanshu-mspl <himanshu.mspl@gmail.com>
2024-08-14 16:32:17 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+st.header('Student Package Prediction')
+
+with open('model.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+# Input from the user
+package = st.number_input("Enter the number of CGPA:")
+
+# Make prediction
+if st.button("Predict"):
+    prediction = model.predict(np.array([[package]]))[0]
+    st.write(f"Predicted student package for {package} CGPA: {prediction:.2f}")
+
+
